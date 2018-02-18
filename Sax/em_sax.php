e72bec44a5bc1ad1f6fa8c3555f3251bbf469bad
Théo <nithwith@live.fr>
2018-02-18 16:51:44 +0100

    Ajout fleuves

--- Sax/em_sax.php
+++ Sax/em_sax.php
@@ -1,51 +1,87 @@
 <?php header('Content-type: text/xml');
 include_once('Sax4PHP/Sax4PHP.php');
 
-class Pays
+class Country
 {
-  public $_nom;
+  public $_name;
   public $_car_code;
-  public $_superficie;
-  public $_nbhab;
+  public $_area;
+  public $_population;
 }
 
-class Fleuve
+class River
 {
   public $_id;
-  public $_nom;
-  public $_longueur;
-  public $_sejette;
+  public $_name;
+  public $_length;
+  public $_flow; //Se jette dans
+  public $_countrys;
+  public $_flow_in_sea;
+  public $_source; //car_code
 }
+
 
 class MySaxHandler extends DefaultHandler {
 
-  public $liste_pays = array();
-  public $pays_temp;
+  public $list_country = array();
+  public $country_temp;
   public $is_country = False;
   public $is_country_name = False;
   public $is_country_population = False;
+
+  public $list_river = array();
+  public $river_temp;
+  public $is_river = False;
+  public $is_river_name = False;
+  public $is_river_to = False;
+  public $is_river_length = False;
 
   function startElement($nom, $att) {
     switch($nom) {
   		case 'country' :
         $this->is_country = True;
-        $this->pays_temp = new Pays();
-        $this->pays_temp->_car_code = $att['car_code'];
-        $this->pays_temp->_superficie = $att['area'];
-
-
+        $this->country_temp = new Country();
+        $this->country_temp->_car_code = $att['car_code'];
+        $this->country_temp->_area = $att['area'];
   			break;
 
   		case 'name' :
         if($this->is_country){
-          if(empty($this->pays_temp->_nom))
+          if(empty($this->country_temp->_name))
           $this->is_country_name = True;
+        }
+        if($this->is_river){
+          if(empty($this->river_temp->_name))
+          $this->is_river_name = True;
         }
         break;
 
       case 'population' :
         if($this->is_country){
           $this->is_country_population = True;
+        }
+        break;
+
+      case 'river' :
+        $this->is_river = True;
+        $this->river_temp = new Country();
+        $this->river_temp->_id = $att['id'];
+        $this->river_temp->_countrys = $att['country'];
+        break;
+      case 'to' :
+        if($this->is_river){
+          $this->river_temp->_flow_in_sea = $att['watertype'];
+          $this->river_temp->_flow = $att['water'];
+        }
+        break;
+      case 'length' :
+      if($this->is_river){
+        $this->is_river_length = True;
+        }
+        break;
+      case 'source' :
+        if($this->is_river){
+          $this->river_temp->_source = $att['country'];
         }
         break;
   		default :;
@@ -55,8 +91,7 @@ class MySaxHandler extends DefaultHandler {
   function endElement($nom) {
     switch($nom) {
   		case 'country':
-
-        $this->liste_pays[] = $this->pays_temp;
+        $this->list_country[] = $this->country_temp;
         break;
       case 'name':
         $this->is_country_name=False;
@@ -64,17 +99,32 @@ class MySaxHandler extends DefaultHandler {
       case 'population_growth' :
         $this->is_country=False;
         break;
+      case 'river':
+        $this->is_river=False;
+        $this->list_river[] = $this->river_temp;
+        break;
+      case 'to':
+        $this->is_river_to=False;
     }
   }
 
   function characters($txt) {
     if($this->is_country_name){
-      $this->pays_temp->_nom = $txt;
+      $this->country_temp->_name = $txt;
       $this->is_country_name=False;
     }
     if($this->is_country_population){
-      $this->pays_temp->_nbhab = $txt;
+      $this->country_temp->_population = $txt;
       $this->is_country_population=False;
+    }
+
+    if($this->is_river_name){
+      $this->river_temp->_name = $txt;
+      $this->is_river_name=False;
+    }
+    if($this->is_river_length){
+      $this->river_temp->_length = $txt;
+      $this->is_river_length=False;
     }
   }
 
@@ -86,9 +136,14 @@ class MySaxHandler extends DefaultHandler {
   function endDocument() {
     echo "</em>\n";
 
-    foreach ($this->liste_pays as $pays) {
-      //echo "<pays ip_p={$pays->_car_code} nom_p={$pays->_nom} superficie={$pays->_superficie} nbhab={$pays->_nbhab}>\n \n" ;
-      echo "carcode={$pays->_car_code}  nom_p={$pays->_nom} pop ={$pays->_nbhab}\n";
+    foreach ($this->list_country as $country) {
+      //echo "<country ip_p={$country->_car_code} nom_p={$country->_name} superficie={$country->_area} nbhab={$country->_population}>\n \n" ;
+      echo "country : carcode={$country->_car_code} nom_p={$country->_name} sup={$country->_area} pop={$country->_population}\n";
+    }
+    foreach ($this->list_river as $river) {
+      if(!empty($river->_flow_in_sea) && $river->_flow_in_sea == "sea" && !empty($river->_flow)){
+        echo "river : id={$river->_id} nom={$river->_name} longueur={$river->_length} se-jette={$river->_flow} countrys={$river->_countrys} source={$river->_source}\n";
+      }
     }
   }
 }
