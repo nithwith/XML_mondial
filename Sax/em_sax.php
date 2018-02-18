41abacd3b638ad45c0447cb32824e051761d0d26
Théo <nithwith@live.fr>
2018-02-18 15:58:11 +0100

    Dimanche aprem pays fini

--- Sax/em_sax.php
+++ Sax/em_sax.php
@@ -20,33 +20,62 @@ class Fleuve
 class MySaxHandler extends DefaultHandler {
 
   public $liste_pays = array();
+  public $pays_temp;
+  public $is_country = False;
+  public $is_country_name = False;
+  public $is_country_population = False;
 
   function startElement($nom, $att) {
     switch($nom) {
   		case 'country' :
+        $this->is_country = True;
+        $this->pays_temp = new Pays();
+        $this->pays_temp->_car_code = $att['car_code'];
+        $this->pays_temp->_superficie = $att['area'];
 
-        $pays = new Pays();
-        $pays->_car_code = $att['car_code'];
-        $pays->_superficie = $att['car_code'];
 
-        $this->liste_pays[] = $pays;
-
-        echo "$car_code \n";
   			break;
-  		case 'titre' :
-        $this->texte = '';
+
+  		case 'name' :
+        if($this->is_country){
+          if(empty($this->pays_temp->_nom))
+          $this->is_country_name = True;
+        }
+        break;
+
+      case 'population' :
+        if($this->is_country){
+          $this->is_country_population = True;
+        }
         break;
   		default :;
   	}
   }
 
   function endElement($nom) {
+    switch($nom) {
+  		case 'country':
 
+        $this->liste_pays[] = $this->pays_temp;
+        break;
+      case 'name':
+        $this->is_country_name=False;
+        break;
+      case 'population_growth' :
+        $this->is_country=False;
+        break;
+    }
   }
 
   function characters($txt) {
-    $txt = trim($txt);
-    if (!(empty($txt))) $this->texte .= $txt;
+    if($this->is_country_name){
+      $this->pays_temp->_nom = $txt;
+      $this->is_country_name=False;
+    }
+    if($this->is_country_population){
+      $this->pays_temp->_nbhab = $txt;
+      $this->is_country_population=False;
+    }
   }
 
   function startDocument() {
@@ -58,12 +87,13 @@ class MySaxHandler extends DefaultHandler {
     echo "</em>\n";
 
     foreach ($this->liste_pays as $pays) {
-      echo "$pays->_car_code \n";
+      //echo "<pays ip_p={$pays->_car_code} nom_p={$pays->_nom} superficie={$pays->_superficie} nbhab={$pays->_nbhab}>\n \n" ;
+      echo "carcode={$pays->_car_code}  nom_p={$pays->_nom} pop ={$pays->_nbhab}\n";
     }
   }
 }
 
-$xml = file_get_contents('Mondial2015/XML/mondial.xml');
+$xml = file_get_contents('../Mondial2015/XML/mondial.xml');
 $sax = new SaxParser(new MySaxHandler());
 try {
 	$sax->parse($xml);
