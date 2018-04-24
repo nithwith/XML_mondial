00299e109c218a31b149c478f481cf49ffd3d66d
tetopowa <theo.lacroix@live.fr>
2018-04-24 19:17:54 +0200

    ajout du shell et jar de baseX

---
+++ execXquery.sh
@@ -0,0 +1,9 @@
+rm out/*.xml
+echo ---------- SHELL d execution du Distanciel Xquery ----------
+echo ------ Théos ------
+echo ----- Lancement de l exercice : XQUERY -----
+echo ----- Fichier de sorti : xquery.xml -----
+time java -cp BaseX90.jar org.basex.BaseX -o out/xquery.xml Xquery/em_xquery.xq
+echo ----- Fin de l exercice 1 : XQUERY -----
+echo --- Vive la DATA ---
+echo -- Votre app NosDéputés bientôt disponible dans tous les store ! --
